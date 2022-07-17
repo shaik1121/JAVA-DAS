b245be3738863c6a5eea88c6773fb682a11ffd03
shaik1121 <sk.muzakkirhussain@gmail.com>
2022-07-17 14:14:35 +0530

    update

--- Bubble_sorting.java
+++ Bubble_sorting.java
@@ -3,6 +3,7 @@ public class Bubble_sorting {
     public static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
+
         }
     }
     public static void main(String[] args) {
